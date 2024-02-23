c4a9d5553fb7812a36a48995a7d9c552a9a15d62
cyschneck <22159116+cyschneck@users.noreply.github.com>
2024-02-23 15:46:56 -0700

    find discontinuities in data

--- data/convert_ppowers_csv.py
+++ data/convert_ppowers_csv.py
@@ -10,7 +10,9 @@ with open("ppowers_raw.txt", "r") as f:
 		line = line.split(" ")
 		minutes_to_seconds = 60*int(line[2])
 		line[3] = int(line[3])
-		if int(line[2]) < 0: line[3] = -line[3]
+		#if int(line[2]) < 0: # for discontinuity
+		if '-' in line[2]: # account for -0 in the raw text
+			line[3] = -line[3]
 		minutes_to_seconds += line[3]
 		output_csv.append([doy, minutes_to_seconds])
 
